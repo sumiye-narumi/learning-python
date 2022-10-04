c19ac70f9c18c5cc16831ce8a06d533e63018ab8
sumiye-narumi <magus5542@gmail.com>
2022-10-04 19:22:57 -0400

    Lesson2 Progress

---
+++ lesson2.py
@@ -0,0 +1,14 @@
+#Cross reference a pair of lists (Validation app given a stored user list, given an inputed list of users, return both name and position in the list)
+
+from xml.etree.ElementTree import Element
+
+
+ownerRosterList = ["Sumiye","Techno","Budviser"]
+
+userList = [item.strip() for item in input("Enter users seperated by commas: ").split(',')]
+
+def rosterValidation(ownerRosterList):
+    
+
+
+# print(userList)
